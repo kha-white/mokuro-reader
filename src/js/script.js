72206648704c1f010b61adb1804c9b6a52514dc9
ZXY101 <tennershaun@gmail.com>
2023-07-28 16:53:38 +0200

    Run prettier over all files

--- src/js/script.js
+++ src/js/script.js
@@ -1,45 +1,56 @@
-import {Catalog} from "./catalog.js";
-import {toggleFullScreen} from "./utils.js";
-import {setIcons} from "./icons";
-import {updateCatalogDisplay} from "./catalog-ui";
-import {modifySettingsValue, updateProperties, updateSettingsDisplay} from "./settings-ui";
-import {initPanzoom, zoomDefault, zoomFitToScreen, zoomFitToWidth, zoomOriginal} from "./panzoom-utils";
-import {closePopup, openPopup, togglePopup} from "./popup";
-import {version} from '../../package.json';
+import { Catalog } from "./catalog.js";
+import { toggleFullScreen } from "./utils.js";
+import { setIcons } from "./icons";
+import { updateCatalogDisplay } from "./catalog-ui";
 import {
-    firstPage,
-    inputLeft,
-    inputLeftLeft,
-    inputRight,
-    inputRightRight,
-    lastPage,
-    nextPage,
-    prevPage,
-    volumeLeft,
-    volumeRight
+  modifySettingsValue,
+  updateProperties,
+  updateSettingsDisplay,
+} from "./settings-ui";
+import {
+  initPanzoom,
+  zoomDefault,
+  zoomFitToScreen,
+  zoomFitToWidth,
+  zoomOriginal,
+} from "./panzoom-utils";
+import { closePopup, openPopup, togglePopup } from "./popup";
+import { version } from "../../package.json";
+import {
+  firstPage,
+  inputLeft,
+  inputLeftLeft,
+  inputRight,
+  inputRightRight,
+  lastPage,
+  nextPage,
+  prevPage,
+  volumeLeft,
+  volumeRight,
 } from "./controls";
-import {updatePage} from "./page-utils";
+import { updatePage } from "./page-utils";
 import "./upload";
-import {unselectBox} from "./select-box";
-import {settings} from "./settings";
-import {loadVolume} from "./load-volume";
-import {globalState} from "./state";
-import {importVolume} from "./web-import";
-import {importUrlPrompt} from "./upload";
+import { unselectBox } from "./select-box";
+import { settings } from "./settings";
+import { loadVolume } from "./load-volume";
+import { globalState } from "./state";
+import { importVolume } from "./web-import";
+import { importUrlPrompt } from "./upload";
 
 window.catalog = null;
 window.loadedVolume = null;
 window.numPages = -1;
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register(
-        new URL('../service-worker.js', import.meta.url),
-        {type: 'module', scope: '/'}
-    );
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register(
+    new URL("../service-worker.js", import.meta.url),
+    { type: "module", scope: "/" },
+  );
 }
 
-
-document.addEventListener('DOMContentLoaded', async function () {
+document.addEventListener(
+  "DOMContentLoaded",
+  async function () {
     console.log("mokuro reader " + version);
     setIcons();
 
@@ -52,12 +63,12 @@ document.addEventListener('DOMContentLoaded', async function () {
     updateProperties();
 
     if (globalState.lastOpened === null) {
-        await loadDemo();
-        openPopup("popupAbout");
+      await loadDemo();
+      openPopup("popupAbout");
     } else if (globalState.loadedVolumeId === null) {
-        openPopup("popupCatalog");
+      openPopup("popupCatalog");
     } else {
-        await loadVolume(globalState.loadedVolumeId);
+      await loadVolume(globalState.loadedVolumeId);
     }
 
     // openPopup("popupHotkeys");
@@ -66,210 +77,291 @@ document.addEventListener('DOMContentLoaded', async function () {
     // openPopup("popupSettings");
 
     globalState.lastOpened = Date.now();
-
-}, false);
+  },
+  false,
+);
 
 async function loadDemo() {
-    await importVolume("demo/UchiNoNyan'sDiary.mokuro");
-    await loadVolume("demo-volume");
+  await importVolume("demo/UchiNoNyan'sDiary.mokuro");
+  await loadVolume("demo-volume");
 }
 
-document.getElementById('versionDisplay').textContent = version;
+document.getElementById("versionDisplay").textContent = version;
 
-document.getElementById('menuOriginalSize').addEventListener('click', zoomOriginal, false);
-document.getElementById('menuFitToWidth').addEventListener('click', zoomFitToWidth, false);
-document.getElementById('menuFitToScreen').addEventListener('click', zoomFitToScreen, false);
-document.getElementById('menuFullScreen').addEventListener('click', toggleFullScreen, false);
+document
+  .getElementById("menuOriginalSize")
+  .addEventListener("click", zoomOriginal, false);
+document
+  .getElementById("menuFitToWidth")
+  .addEventListener("click", zoomFitToWidth, false);
+document
+  .getElementById("menuFitToScreen")
+  .addEventListener("click", zoomFitToScreen, false);
+document
+  .getElementById("menuFullScreen")
+  .addEventListener("click", toggleFullScreen, false);
 
-document.getElementById('menuAbout').addEventListener('click', function () {
+document.getElementById("menuAbout").addEventListener(
+  "click",
+  function () {
     openPopup("popupAbout");
-}, false);
+  },
+  false,
+);
 
-document.getElementById('menuCatalog').addEventListener('click', function () {
+document.getElementById("menuCatalog").addEventListener(
+  "click",
+  function () {
     openPopup("popupCatalog");
-}, false);
+  },
+  false,
+);
 
-document.getElementById('menuCloseVolume').addEventListener('click', async function () {
+document.getElementById("menuCloseVolume").addEventListener(
+  "click",
+  async function () {
     await loadVolume(null);
-}, false);
+  },
+  false,
+);
 
-document.getElementById('menuSettings').addEventListener('click', function () {
+document.getElementById("menuSettings").addEventListener(
+  "click",
+  function () {
     openPopup("popupSettings");
-}, false);
+  },
+  false,
+);
 
-document.getElementById('chooseFilesButton').addEventListener('click', async function () {
-    document.getElementById('uploadFile').click();
-}, false);
+document.getElementById("chooseFilesButton").addEventListener(
+  "click",
+  async function () {
+    document.getElementById("uploadFile").click();
+  },
+  false,
+);
 
-document.getElementById('chooseDirectoryButton').addEventListener('click', async function () {
-    document.getElementById('uploadDirectory').click();
-}, false);
+document.getElementById("chooseDirectoryButton").addEventListener(
+  "click",
+  async function () {
+    document.getElementById("uploadDirectory").click();
+  },
+  false,
+);
 
-document.getElementById('importUrlButton').addEventListener('click', importUrlPrompt, false);
+document
+  .getElementById("importUrlButton")
+  .addEventListener("click", importUrlPrompt, false);
 
 document.querySelectorAll(".popupCloseButton").forEach(function (x) {
-    x.addEventListener("click", closePopup, false);
+  x.addEventListener("click", closePopup, false);
 });
 
-document.getElementById('dimOverlay').addEventListener('click', function () {
+document.getElementById("dimOverlay").addEventListener(
+  "click",
+  function () {
     closePopup();
-}, false);
+  },
+  false,
+);
 
+document.getElementById("pageIdxInput").addEventListener("change", (e) => {
+  updatePage(e.target.value - 1);
+});
 
-document.getElementById('pageIdxInput').addEventListener('change', (e) => {
-    updatePage(e.target.value - 1);
-})
-
-document.getElementById('openCatalogButton').addEventListener('click', function () {
+document.getElementById("openCatalogButton").addEventListener(
+  "click",
+  function () {
     openPopup("popupCatalog", false);
-}, false);
+  },
+  false,
+);
 
-document.getElementById('hotkeysButton').addEventListener('click', function () {
+document.getElementById("hotkeysButton").addEventListener(
+  "click",
+  function () {
     openPopup("popupHotkeys", false);
-}, false);
+  },
+  false,
+);
 
-document.getElementById('loadDemoButton').addEventListener('click', function () {
+document.getElementById("loadDemoButton").addEventListener(
+  "click",
+  function () {
     loadDemo();
-}, false);
+  },
+  false,
+);
 
-document.getElementById('buttonHideMenu').addEventListener('click', function () {
-    document.getElementById('showMenuA').style.display = "inline-block";
-    document.getElementById('topMenu').classList.add("hidden");
-}, false);
+document.getElementById("buttonHideMenu").addEventListener(
+  "click",
+  function () {
+    document.getElementById("showMenuA").style.display = "inline-block";
+    document.getElementById("topMenu").classList.add("hidden");
+  },
+  false,
+);
 
-document.getElementById('showMenuA').addEventListener('click', function () {
-    document.getElementById('showMenuA').style.display = "none";
-    document.getElementById('topMenu').classList.remove("hidden");
-}, false);
+document.getElementById("showMenuA").addEventListener(
+  "click",
+  function () {
+    document.getElementById("showMenuA").style.display = "none";
+    document.getElementById("topMenu").classList.remove("hidden");
+  },
+  false,
+);
 
-document.getElementById('buttonLeftLeft').addEventListener('click', inputLeftLeft, false);
-document.getElementById('buttonLeft').addEventListener('click', inputLeft, false);
-document.getElementById('buttonRight').addEventListener('click', inputRight, false);
-document.getElementById('buttonRightRight').addEventListener('click', inputRightRight, false);
-document.getElementById('leftAPage').addEventListener('click', inputLeft, false);
-document.getElementById('leftAScreen').addEventListener('click', inputLeft, false);
-document.getElementById('rightAPage').addEventListener('click', inputRight, false);
-document.getElementById('rightAScreen').addEventListener('click', inputRight, false);
+document
+  .getElementById("buttonLeftLeft")
+  .addEventListener("click", inputLeftLeft, false);
+document
+  .getElementById("buttonLeft")
+  .addEventListener("click", inputLeft, false);
+document
+  .getElementById("buttonRight")
+  .addEventListener("click", inputRight, false);
+document
+  .getElementById("buttonRightRight")
+  .addEventListener("click", inputRightRight, false);
+document
+  .getElementById("leftAPage")
+  .addEventListener("click", inputLeft, false);
+document
+  .getElementById("leftAScreen")
+  .addEventListener("click", inputLeft, false);
+document
+  .getElementById("rightAPage")
+  .addEventListener("click", inputRight, false);
+document
+  .getElementById("rightAScreen")
+  .addEventListener("click", inputRight, false);
 
 document.addEventListener("keydown", async function onEvent(e) {
-    let activeElement = document.activeElement;
-    if (activeElement.contentEditable === "true") {
-        return;
-    }
+  let activeElement = document.activeElement;
+  if (activeElement.contentEditable === "true") {
+    return;
+  }
 
-    switch (e.key) {
-        case "ArrowLeft":
-            if (settings.turnPagesWithArrows) {
-                await inputLeft();
-            }
-            break;
+  switch (e.key) {
+    case "ArrowLeft":
+      if (settings.turnPagesWithArrows) {
+        await inputLeft();
+      }
+      break;
 
-        case "ArrowRight":
-            if (settings.turnPagesWithArrows) {
-                await inputRight();
-            }
-            break;
+    case "ArrowRight":
+      if (settings.turnPagesWithArrows) {
+        await inputRight();
+      }
+      break;
 
-        case "ArrowUp":
-            if (settings.turnPagesWithArrows) {
-                await prevPage();
-            }
-            break;
+    case "ArrowUp":
+      if (settings.turnPagesWithArrows) {
+        await prevPage();
+      }
+      break;
 
-        case "ArrowDown":
-            if (settings.turnPagesWithArrows) {
-                await nextPage();
-            }
-            break;
+    case "ArrowDown":
+      if (settings.turnPagesWithArrows) {
+        await nextPage();
+      }
+      break;
 
-        case "PageUp":
-            await prevPage();
-            break;
+    case "PageUp":
+      await prevPage();
+      break;
 
-        case "PageDown":
-            await nextPage();
-            break;
+    case "PageDown":
+      await nextPage();
+      break;
 
-        case "Home":
-            firstPage();
-            break;
+    case "Home":
+      firstPage();
+      break;
 
-        case "End":
-            lastPage();
-            break;
+    case "End":
+      lastPage();
+      break;
 
-        case " ":
-            await nextPage();
-            break;
+    case " ":
+      await nextPage();
+      break;
 
-        case "0":
-            zoomDefault();
-            break;
+    case "0":
+      zoomDefault();
+      break;
 
-        case ",":
-            await volumeLeft();
-            break;
+    case ",":
+      await volumeLeft();
+      break;
 
-        case ".":
-            await volumeRight();
-            break;
+    case ".":
+      await volumeRight();
+      break;
 
-        case "c":
-            togglePopup("popupCatalog");
-            break;
+    case "c":
+      togglePopup("popupCatalog");
+      break;
 
-        case "s":
-            togglePopup("popupSettings");
-            break;
+    case "s":
+      togglePopup("popupSettings");
+      break;
 
-        case "Escape":
-            closePopup();
-            break;
+    case "Escape":
+      closePopup();
+      break;
 
-        case "f":
-            toggleFullScreen();
-            break;
+    case "f":
+      toggleFullScreen();
+      break;
 
-        case "h":
-            togglePopup("popupAbout");
-            break;
+    case "h":
+      togglePopup("popupAbout");
+      break;
 
-        case "x":
-            await loadVolume(null);
-            break;
+    case "x":
+      await loadVolume(null);
+      break;
 
-        case "d":
-            if (window.loadedVolume !== null) {
-                modifySettingsValue("volume", "doublePageView", !settings.doublePageView);
-            }
-            break;
+    case "d":
+      if (window.loadedVolume !== null) {
+        modifySettingsValue(
+          "volume",
+          "doublePageView",
+          !settings.doublePageView,
+        );
+      }
+      break;
 
-        case "j":
-            if (window.loadedVolume !== null) {
-                modifySettingsValue("volume", "r2l", !settings.r2l);
-            }
-            break;
+    case "j":
+      if (window.loadedVolume !== null) {
+        modifySettingsValue("volume", "r2l", !settings.r2l);
+      }
+      break;
 
-        case "p":
-            if (window.loadedVolume !== null) {
-                modifySettingsValue("volume", "hasCover", !settings.hasCover);
-            }
-            break;
+    case "p":
+      if (window.loadedVolume !== null) {
+        modifySettingsValue("volume", "hasCover", !settings.hasCover);
+      }
+      break;
 
-        case "r":
-            if (window.loadedVolume !== null) {
-                settings.resetVolume();
-                updateSettingsDisplay();
-                updateProperties();
-                updatePage();
-            }
-            break;
-
-    }
+    case "r":
+      if (window.loadedVolume !== null) {
+        settings.resetVolume();
+        updateSettingsDisplay();
+        updateProperties();
+        updatePage();
+      }
+      break;
+  }
 });
 
-document.addEventListener('click', function (e) {
-    if (e.target.closest('.textBox') === null) {
-        unselectBox();
+document.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.closest(".textBox") === null) {
+      unselectBox();
     }
-}, false);
+  },
+  false,
+);
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,113 +1,137 @@
 import natsort from "natsort";
 
 export function appendNewChild(parentElement, tagName, text = null) {
-    let child = document.createElement(tagName);
-    parentElement.appendChild(child);
-    if (text !== null) {
-        child.appendChild(document.createTextNode(text));
-    }
-    return child;
+  let child = document.createElement(tagName);
+  parentElement.appendChild(child);
+  if (text !== null) {
+    child.appendChild(document.createTextNode(text));
+  }
+  return child;
 }
 
 function normalize(inputString) {
-    if (typeof inputString !== 'string') {
-        return inputString;
-    }
+  if (typeof inputString !== "string") {
+    return inputString;
+  }
 
-    let replacements = {
-        '０': '0',
-        '１': '1',
-        '２': '2',
-        '３': '3',
-        '４': '4',
-        '５': '5',
-        '６': '6',
-        '７': '7',
-        '８': '8',
-        '９': '9'
-    };
+  let replacements = {
+    "０": "0",
+    "１": "1",
+    "２": "2",
+    "３": "3",
+    "４": "4",
+    "５": "5",
+    "６": "6",
+    "７": "7",
+    "８": "8",
+    "９": "9",
+  };
 
-    return inputString.replace(/[０１２３４５６７８９]/g, function (match) {
-        return replacements[match];
-    });
+  return inputString.replace(/[０１２３４５６７８９]/g, function (match) {
+    return replacements[match];
+  });
 }
 
+export function sortByProperty(
+  arr,
+  property,
+  ascending = true,
+  natural = false,
+) {
+  let x;
+  if (ascending) {
+    x = 1;
+  } else {
+    x = -1;
+  }
 
-export function sortByProperty(arr, property, ascending = true, natural = false) {
-    let x;
-    if (ascending) {
-        x = 1;
-    } else {
-        x = -1;
-    }
+  if (natural) {
+    let sorter = natsort();
+    return arr.sort((a, b) =>
+      sorter(normalize(a[property]), normalize(b[property])),
+    );
+  }
 
-    if (natural) {
-        let sorter = natsort();
-        return arr.sort((a, b) => sorter(normalize(a[property]), normalize(b[property])));
-    }
-
-    return arr.sort((a, b) => (normalize(a[property]) > normalize(b[property])) ? x : ((normalize(b[property]) > normalize(a[property])) ? -x : 0));
+  return arr.sort((a, b) =>
+    normalize(a[property]) > normalize(b[property])
+      ? x
+      : normalize(b[property]) > normalize(a[property])
+      ? -x
+      : 0,
+  );
 }
 
 export function clip(x, min, max) {
-    return Math.min(Math.max(x, min), max);
+  return Math.min(Math.max(x, min), max);
 }
 
-
 export function toggleFullScreen() {
-    let doc = window.document;
-    let docEl = doc.documentElement;
+  let doc = window.document;
+  let docEl = doc.documentElement;
 
-    let requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
-    let cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+  let requestFullScreen =
+    docEl.requestFullscreen ||
+    docEl.mozRequestFullScreen ||
+    docEl.webkitRequestFullScreen ||
+    docEl.msRequestFullscreen;
+  let cancelFullScreen =
+    doc.exitFullscreen ||
+    doc.mozCancelFullScreen ||
+    doc.webkitExitFullscreen ||
+    doc.msExitFullscreen;
 
-    if (!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
-        requestFullScreen.call(docEl);
-    } else {
-        cancelFullScreen.call(doc);
-    }
+  if (
+    !doc.fullscreenElement &&
+    !doc.mozFullScreenElement &&
+    !doc.webkitFullscreenElement &&
+    !doc.msFullscreenElement
+  ) {
+    requestFullScreen.call(docEl);
+  } else {
+    cancelFullScreen.call(doc);
+  }
 }
 
 export function load(key, getDefaultFn) {
-    let state = getDefaultFn();
-    let stateLoaded = localStorage.getItem(key)
+  let state = getDefaultFn();
+  let stateLoaded = localStorage.getItem(key);
 
-    if (stateLoaded !== null) {
-        stateLoaded = JSON.parse(stateLoaded);
-        for (const k of Object.keys(state)) {
-            if (k in stateLoaded) {
-                state[k] = stateLoaded[k];
-            }
-        }
+  if (stateLoaded !== null) {
+    stateLoaded = JSON.parse(stateLoaded);
+    for (const k of Object.keys(state)) {
+      if (k in stateLoaded) {
+        state[k] = stateLoaded[k];
+      }
     }
+  }
 
-    return state;
+  return state;
 }
 
 export function save(key, state) {
-    localStorage.setItem(key, JSON.stringify(state));
+  localStorage.setItem(key, JSON.stringify(state));
 }
 
 export async function readJSON(file) {
-    let text = await file.text();
-    return JSON.parse(text);
+  let text = await file.text();
+  return JSON.parse(text);
 }
 
 export function formatBytes(bytes, decimals = 2) {
-    if (bytes === 0) return '0 B';
+  if (bytes === 0) return "0 B";
 
-    const k = 1024;
-    const dm = decimals < 0 ? 0 : decimals;
-    const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
 
-    const i = Math.floor(Math.log(bytes) / Math.log(k));
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
 
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
 }
 
 export async function requestPersistentStorage() {
-    if (navigator.storage && navigator.storage.persist) {
-        const isPersisted = await navigator.storage.persist();
-        console.log(`Persisted storage granted: ${isPersisted}`);
-    }
+  if (navigator.storage && navigator.storage.persist) {
+    const isPersisted = await navigator.storage.persist();
+    console.log(`Persisted storage granted: ${isPersisted}`);
+  }
 }
